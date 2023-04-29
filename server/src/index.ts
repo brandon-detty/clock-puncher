90f36976bed6a782141a1b744876772eefc582b7
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2023-04-28 20:21:14 -0700

    basic koa setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+import koa from 'koa';
+import koaRouter from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import json from 'koa-json';
+
+const app = new koa();
+
+app.use(json());
+app.use(bodyParser());
+
+const router = new koaRouter();
+
+router.get('/', async (ctx, next) => {
+  ctx.body = { msg: 'Hello, world!' };
+
+  await next();
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(5000);
